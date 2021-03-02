1e2374e16c9454058b74c13b9030861afe12379f
armanuzzamankuetcse@gmail.com <armanuzzamankuetcse@gmail.com>
2021-03-02 13:40:11 -0500

    generate RSA key header file for each SED

--- sss/create_secret.py
+++ sss/create_secret.py
@@ -31,57 +31,69 @@ def del_input(scewl_id):
     else:
         print("failure: public and/or private key does not exist for ", scewl_id)
 
+def write_into_header_file(struct_name, key_file_header, list_of_key_values, name, int_index):
+    last_index = len(list_of_key_values) - 1
+
+    key_file_header.write(f'''
+{struct_name} {name} = {{
+        ''')
+
+    cnt = 0
+    for line_list in list_of_key_values:
+        if cnt < int_index:
+            key_file_header.write(f'''
+    {{{", ".join(['0x' + u for u in line_list])}}},
+            ''')
+        elif cnt == last_index:
+            dValue = ''.join(line_list)
+            key_file_header.write(f'''
+    0x{dValue}
+            ''')
+        else:
+            dValue = ''.join(line_list)
+            key_file_header.write(f'''
+    0x{dValue},
+            ''')
+        cnt = cnt + 1
+    key_file_header.write(f'''
+}};
+  
+    ''')
 
 def enc_input(scewl_id):
-    #TODO: encrypt input -> return public pri key
-    #should write to specific files
-    (scewl_pub, scewl_pri) = rsa.newkeys(512)
+    key_file_name = "/" + str(scewl_id) + "/key.h"
     
-    #get filenames
-    fn_pub = str(scewl_id) + ".pub"
-    fn_pri = str(scewl_id) + ".pri"
+    private_key_file_path = "/" + str(scewl_id) + "/privateKey.txt"
+    public_key_file_path = "/" + str(scewl_id) + "/publicKey.txt"
+
+    key_file_header = open(key_file_name, "w")
+    privateKey_file = open(private_key_file_path, 'r')
+    publicKey_file = open(public_key_file_path, 'r')
+
+    pirvateKey_Lines = privateKey_file.readlines()
+    publicKey_Lines = publicKey_file.readlines()
+
+    cnt = 0
+    for line in pirvateKey_Lines:
+        line = line.replace('\n', '')
+        pirvateKey_Lines[cnt] = [(line[i:i+4]) for i in range(0, len(line), 4)]
+        cnt = cnt + 1
     
-    #write to pub file
-    myfile_pub = open(fn_pub, 'w')
-    print(type(scewl_pub))
-    # myfile_pub.write(str(scewl_pub))
-    myfile_pub.write(scewl_pub.save_pkcs1().decode())
-
-    myfile_pub.close()
-
-
-    ########################02/28 add public key transform
-    #   Qiqing Huang note:
-    #   add this part is because the public key format in controller is in the format of  modulus (N) plus publicExponent (E). But the rsa python api for loading the key needs the PEM DER ASN.1 PKCS#1 format. So there we do a transformation from PEM DER ASN.1 PKCS#1 to just modulus (N) plus publicExponent (E)
-    #   the details of public key format can be found in https://stackoverflow.com/questions/18039401/how-can-i-transform-between-the-two-styles-of-public-key-format-one-begin-rsa
-    with open(fn_pub, mode='rb') as publicfile:
-        keydata = publicfile.read()
-        # pubkey = rsa.PublicKey.load_pkcs1(keydata)
-        test = rsa.PublicKey.load_pkcs1(keydata)
-        # print(test)
-        module_s = str(test).split("(")[1].split(",")[0].strip()
-        publicExponent_s = str(test).split(")")[0].split(",")[1].strip()
-
-        module = format(int(module_s), 'x')
-        publicExponent = format(int(publicExponent_s), 'x').zfill(8)
-
-        pubkeyForSend = open(fn_pub + "_send", "w")
-        pubkeyForSend.write(module)
-        pubkeyForSend.write("\r\n")
-        pubkeyForSend.write(publicExponent)
-        pubkeyForSend.close()
-
-    ###############################02/28 add public key transform
-
-
-
-    #write to pri file
-    f_pri = open(fn_pri, 'w')
-    # f_pri.write(str(scewl_pri))
-    f_pri.write(scewl_pri.save_pkcs1().decode())
-    f_pri.close()
+    cnt = 0
+    for line in publicKey_Lines:
+        line = line.replace('\n', '')
+        publicKey_Lines[cnt] = [(line[i:i+4]) for i in range(0, len(line), 4)]
+        cnt = cnt + 1
     
-    print("success: created private and public keys for ", str(scewl_id))
+    key_file_header.write(f'''
+#ifndef KEY_H
+#define KEY_H
+#include "keys.h"
+#endif
+
+    ''')
+    write_into_header_file("rsa_sk", key_file_header, pirvateKey_Lines, "private_key", 11)
+    write_into_header_file("rsa_pk", key_file_header, publicKey_Lines, "public_key", 3)
 
 if __name__ == '__main__':
     #read input from command line
