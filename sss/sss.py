3b03a5d99854e00ecd514abac8246959678f823e
armanuzzamankuetcse@gmail.com <armanuzzamankuetcse@gmail.com>
2021-03-09 15:44:31 -0500

    clean up sss.py

--- sss/sss.py
+++ sss/sss.py
@@ -60,7 +60,7 @@ class SSS:
         legit_SED_flag = False
         data = b''
         already_registered_sed = 0
-        
+        # the registration message will be signed by scewl's key and the length would be 64 bytes + 8 bytes of the header (72 bytes)
         while len(data) < 72:
             recvd = csock.recv(72 - len(data))
             data += recvd
@@ -70,22 +70,19 @@ class SSS:
                 raise ConnectionResetError
 
         logging.info(f'Received buffer: {repr(data)}')
-        logging.info(f'Lenght of data: {len(data)}')
+        #logging.info(f'Lenght of data: {len(data)}')
         _, target_id, src_id, _ = struct.unpack('<HHHH', data[:8])
         logging.info(f'header target: {target_id} header src: {src_id}')
         
         data = data[8:]
         logging.info(f'Received buffer: {repr(data)}')
-        logging.info(f'Lenght of data: {len(data)}')
-        logging.info(f'Lenght of data: {type(data)}')
-        #op = 0 #remove
+        #logging.info(f'Lenght of data: {len(data)}')
+        #logging.info(f'Lenght of data: {type(data)}')
+        
         cipher_file = open("rsa/cipher", "wb")
         #cipher_file = open("cipher", "wb")
         cipher_file.write(data)
         cipher_file.close()
-        cipher_file = open("rsa/cipher", "rb").read()
-        logging.info(f'written data : {repr(cipher_file)}')
-        #cipher_file.close()
         #logging.info(f'source: {src_id}')
         auth_app_command = "./rsa/auth " + str(src_id)
 
@@ -118,12 +115,14 @@ class SSS:
         else:
             logging.info(f'In valid Reigstration for SED')
             dev_id = target_id
+            #resp_op = ALREADY
 
         logging.info(f'dev ID:{d_dev_id} op: {d_op} target: {d_target_id} src: {d_src_id}')
         
         if dev_id in provisionedList:
             provisioned_flag = True
             logging.info(f'ID: {dev_id} beloings to provisioned list')
+            #resp_op = ALREADY
         
         
         if (provisioned_flag and legit_SED_flag) :
@@ -206,65 +205,12 @@ def parse_args():
     parser.add_argument('sockf', help='Path to socket to bind the SSS to')
     return parser.parse_args()
 
-# input signed message
+
 
 #########################################################################
-### 2021/2/22 modified by Qiqing Huang
-### task5 registration and deregistration 
 
-#   header = It has 2 bytes of magic number, 2 bytes of destination SCEWL ID, 2 bytes of source SCEWL ID, 2 bytes of body length in bytes.
-
-#   message  = header + signed(header + device id + op)
-#   message = header + signed(header + device id+ op) + device id + operation 
-
-def createTmpMsg(deviceID):
-    
-    privateKey = str(deviceID) + ".pri"
-    magicNumber = 0
-    OP = 1
-    descID = 0
-    sourceID = 1
-    length = 12
-
-    header = struct.pack('!HHHH', magicNumber, descID, sourceID, length) 
-
-    # _, _, _, _, dev_id, op = struct.unpack('<HHHHHH', data)
-    # print(header)
-
-    header_l = struct.unpack('!Q', header)
-
-    # print(header_l[0])
-
-    signedBody = struct.pack('!QHH', header_l[0], deviceID, OP)
-
-    # print(signedBody)
-
-    # writeFile  = open("tmp", mode='wb+')
-    # writeFile.write(signedBody)
-
-    with open(privateKey, mode='rb') as privatefile:
-        keydata = privatefile.read()
-        privkey = rsa.PrivateKey.load_pkcs1(keydata)
-
-    encSignedBody = rsa.sign(signedBody, privkey, 'MD5') 
-
-    # writeFile.close()
-    # os.remove()
-    # print(header)
-    # print(type(encSignedBody))
-
-    # signedMsg = str(header) + str(encSignedBody) + str(12) + str(OP)
-
-    signedMsg = b"".join([header, encSignedBody, (deviceID).to_bytes(4, 'big'), OP.to_bytes(4, 'big')])
-
-
-
-    # signedMsg = struct.pack('<ss', header, encSignedBody)
-    print("signedMsg" + str(signedMsg) + "\r\n")
-
-    return signedMsg
-
-
+# The docker files to build the SSS and controller will create a (provisoned_list) file in SSS reading this file we check wheather
+#SED with registration request belongs to the provisioned list or not
 def preapred_provisioned_list():
         provisionedFile = open("../provisoned_list", "r")
         provisionedSEDLines = provisionedFile.readlines()
@@ -274,109 +220,8 @@ def preapred_provisioned_list():
         logging.info(f'Provisioned SEDS: {provisionedList}')
         provisionedFile.close()
 
-def registration_tmp(signedMsg):   
-
-    ### STEP1 read the SCEWL_ID of the signed message
-    # OP = signedMsg[-1:]
-    # deviceID = signedMsg[187:-1]
-    OP = signedMsg[-4:]
-    OP_i = 0
-    OP_i = int.from_bytes(OP, byteorder='big', signed=False)
-
-    deviceID_i = 0
-    deviceID = signedMsg[-8:-4]
-    # print(deviceID)
-    deviceID_i=int.from_bytes(deviceID, byteorder='big', signed=False)
-
-    ### STEP2 check whether the ID is a provisioned SED or no
-    if(deviceID_i in provisionedList):    
-
-        ### STEP3 verify the header
-        header = signedMsg[:8]
-        # print(header)
-        # header = It has 2 bytes of magic number, 2 bytes of destination SCEWL ID, 2 bytes of source SCEWL ID, 2 bytes of body length in bytes.
-        magicNumber, descID, sourceID, length = struct.unpack('!hhhh', header)
-
-        encSignedBody = signedMsg[8:-8]
-
-        header_l = struct.unpack('!Q', header)
-
-        verifyBody = struct.pack('!QHH', header_l[0], deviceID_i, OP_i)
-
-
-        publicKey = str(deviceID_i) + ".pub"
-        with open(publicKey, mode='rb') as publicfile:
-            keydata = publicfile.read()
-            pubkey = rsa.PublicKey.load_pkcs1(keydata)
-            
-
-        try:
-            result = rsa.verify(verifyBody, encSignedBody, pubkey)
-            ### STEP4 store the id to the dedicated list
-            if(result):
-                dedicatedList.append(deviceID_i)
-
-                print("provisionedList: ")
-                print(provisionedList)
-                print("dedicatedList: ")
-                print(dedicatedList)
-
-                print(str(deviceID_i) + " registration done!\r\n")
-                ### NEED TO DO STEP5 read local public key files and send them to the SED
-
-                #the public key file need to send is:
-                #   str(deviceID_i) + "_send" + ".pub"
-                sendAllPubKeys()
-
-        except:
-            return False
-    
-    return
-
-def deregistration_tmp(signedMsg):
-    ### STEP1 read the SCEWL_ID of the signed message
-
-    OP = signedMsg[-4:]
-    OP_i = 0
-    OP_i = int.from_bytes(OP, byteorder='big', signed=False)
-
-    deviceID_i = 0
-    deviceID = signedMsg[-8:-4]
-    deviceID_i=int.from_bytes(deviceID, byteorder='big', signed=False)
-    ### STEP2 check whether the ID is a provisioned SED & in the dedicated list or not
-    if(deviceID_i in provisionedList and deviceID_i in dedicatedList): 
-        header = signedMsg[:8]
-        magicNumber, descID, sourceID, length = struct.unpack('!hhhh', header)
-
-        encSignedBody = signedMsg[8:-8]
-
-        header_l = struct.unpack('!Q', header)
-
-        verifyBody = struct.pack('!QHH', header_l[0], deviceID_i, OP_i)
-
-
-        publicKey = str(deviceID_i) + ".pub"
-        with open(publicKey, mode='rb') as publicfile:
-            keydata = publicfile.read()
-            pubkey = rsa.PublicKey.load_pkcs1(keydata)
-
-        try:
-            ### STEP3 verify the header
-            result = rsa.verify(verifyBody, encSignedBody, pubkey)
-            ### STEP4 remove the ID from the dedicated list
-            if(result):
-                dedicatedList.remove(deviceID_i)
-
-                print("provisionedList: ")
-                print(provisionedList)
-                print("dedicatedList: ")
-                print(dedicatedList)
-
-                print(str(deviceID_i) + " deregistration done!\r\n")
-        except:
-            return False
-    return 
-
+#For each SED there will be public key file in SSS container with the name SED_ID_publickey in /rsa folder here we just read the file
+#and return the values to the caller
 def get_publicKey(registed_SED_id):
     public_key_file_path = "rsa/" + str(registed_SED_id) + "_publicKey"
     logging.info(f'public_key_file_path {public_key_file_path}')
@@ -386,6 +231,7 @@ def get_publicKey(registed_SED_id):
 
     return pub_key_file_data
 
+# From the registered list preare the response with SED ID(2 byte) + public key(162 byte) for each previously reistered SED
 def prepare_response(registered_sed_list):
     resp = b''
     for registered_sed_id in registered_sed_list:
@@ -396,40 +242,7 @@ def prepare_response(registered_sed_list):
     return resp
 
 
-def sendAllPubKeys():
-    for deviceID_i in provisionedList:
-        pubKeyFileName = str(deviceID_i) + ".pub_send"
-        readFile = open(pubKeyFileName, "r")
-        module_s = readFile.readline().strip()
-        publicExponent_s = readFile.readline().strip()
-
-
-        module_bytes = bytes(module_s, 'utf-8') 
-        publicKey_bin = struct.pack("I%dsH" % (len(module_bytes),), len(module_bytes), module_bytes, deviceID_i)
-        # Qiqing Huang note:
-        # Here I found the public exponent is always 00010001, so just need to send out the module_bin
-        # print(publicKey_bin)
-        # pending test of the csock.send()
-        csock.send(publicKey_bin)
-
-
-
-
 def main():
-
-    ### registration_tmp and deregistration_tmp test
-    # signedMsg = createTmpMsg(0)
-    # registration_tmp(signedMsg)
-    # deregistration_tmp(signedMsg)
-    ### registration_tmp and deregistration_tmp test
-
-    
-    ### test of sending all public keys
-    # sendAllPubKeys()
-    ### test of sending all public keys
-
-
-
 
     args = parse_args()
     # map of SCEWL IDs to statuses
