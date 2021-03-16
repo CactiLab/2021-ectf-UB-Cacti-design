e1804e0c8564bdc9bb19f95bd7d2374ccdfcdde0
mintancy <mintancy@hotmail.com>
2021-03-16 22:18:26 +0000

    test_multi_reg

--- cpu/seds/client1/main.c
+++ cpu/seds/client1/main.c
@@ -70,6 +70,7 @@ int main(void) {
 
     i ++;
   }
+  i = 0;
 
   // deregister
   fprintf(log, "Deregistering...\n");
@@ -77,4 +78,101 @@ int main(void) {
     fprintf(log, "BAD DEREGISTRATION!\n");
   }
   fprintf(log, "Exiting...\n");
+
+  // register2
+  if (scewl_register() != SCEWL_OK) {
+    fprintf(log, "BAD REGISTRATION! Reregistering...\n");
+    if (scewl_deregister() != SCEWL_OK) {
+      fprintf(log, "BAD DEREGISTRATION!\n");
+      return 1;
+    }
+    if (scewl_register() != SCEWL_OK) {
+      fprintf(log, "BAD REGISTRATION! CANNOT RECOVER\n");
+      return 1;
+    }
+  }
+
+  fprintf(log, "Sending hello...\n");
+  scewl_send(TGT_ID, 14, msg);
+
+  // receive response (block until response received)
+  fprintf(log, "Waiting for response...\n");
+  scewl_recv(data, &src_id, &tgt_id, BUF_SZ, 1);
+
+  while (i< 10) { 
+    
+    scewl_send(TGT_ID, 13, msg);
+
+    // receive response (block until response received)
+    fprintf(log, "Waiting for response...\n");
+    scewl_recv(data, &src_id, &tgt_id, BUF_SZ, 1);
+
+    fprintf(log, "receive response...\n");
+
+    i ++;
+  }
+  i = 0;
+
+  // deregister2
+  fprintf(log, "Deregistering...\n");
+  if (scewl_deregister() != SCEWL_OK) {
+    fprintf(log, "BAD DEREGISTRATION!\n");
+  }
+  fprintf(log, "Exiting...\n");
+
+  // register3
+  if (scewl_register() != SCEWL_OK) {
+    fprintf(log, "BAD REGISTRATION! Reregistering...\n");
+    if (scewl_deregister() != SCEWL_OK) {
+      fprintf(log, "BAD DEREGISTRATION!\n");
+      return 1;
+    }
+    if (scewl_register() != SCEWL_OK) {
+      fprintf(log, "BAD REGISTRATION! CANNOT RECOVER\n");
+      return 1;
+    }
+  }
+
+  fprintf(log, "Sending hello...\n");
+  scewl_send(TGT_ID, 14, msg);
+
+  // receive response (block until response received)
+  fprintf(log, "Waiting for response...\n");
+  scewl_recv(data, &src_id, &tgt_id, BUF_SZ, 1);
+
+  while (i< 10) { 
+    
+    scewl_send(TGT_ID, 13, msg);
+
+    // receive response (block until response received)
+    fprintf(log, "Waiting for response...\n");
+    scewl_recv(data, &src_id, &tgt_id, BUF_SZ, 1);
+
+    fprintf(log, "receive response...\n");
+
+    i ++;
+  }
+  i = 0;
+
+  // deregister3
+  fprintf(log, "Deregistering...\n");
+  if (scewl_deregister() != SCEWL_OK) {
+    fprintf(log, "BAD DEREGISTRATION!\n");
+  }
+  fprintf(log, "Exiting...\n");
+
+  // deregister4
+  fprintf(log, "Deregistering...\n");
+  if (scewl_deregister() != SCEWL_OK) {
+    fprintf(log, "BAD DEREGISTRATION!\n");
+  }
+  fprintf(log, "Exiting...\n");
+
+    // deregister5
+  fprintf(log, "Deregistering...\n");
+  if (scewl_deregister() != SCEWL_OK) {
+    fprintf(log, "BAD DEREGISTRATION!\n");
+  }
+  fprintf(log, "Exiting...\n");
+
 }
